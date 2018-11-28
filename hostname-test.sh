59a174d365b6f9b908e9643fa89eb7b8936aa736
colleen-work <crooney@pdx.edu>
2018-11-28 15:22:28 -0800

    fixed bugs and condensed script

--- hostname-test.sh
+++ hostname-test.sh
@@ -3,15 +3,14 @@
 
 OUTPUT_DIR=hostname-output
 
-nodes=( $(./get-idle-nodes.py -v -p medium) )
+nodes=( $(./get-idle-nodes.py -v) )
 num_idle_array=${#nodes[@]}
 
+mkdir -p $OUTPUT_DIR
+rm -rf $OUTPUT_DIR/*
 cp hostname.sh temp.sh
 
-num_idle_array="$(($num_idle_array))"
-
-for (( i=0; i<${#nodes[@]}; i+=2)) ;
-do
+for (( i=0; i<$num_idle_array; i+=2)); do
     node=${nodes[$i]}
     partition=${nodes[$i + 1]}
     name="${node//[0-9]/}"
@@ -21,27 +20,18 @@ done
 
 rm temp_sub.sh temp.sh
 
-echo $files
-while [  $(ls hostname-output | wc -l) -lt $num_idle_array ];
-do
+while [  $(ls hostname-output | wc -l) -lt $num_idle_array ]; do
     echo waiting for output
 done
 
-for log in $OUTPUT_DIR/*.log;
-do
-    # remove log files
-    rm $log 
-done
+rm $OUTPUT_DIR/*.log
 
-for err in $OUTPUT_DIR/*.err;
-do
+for err in $OUTPUT_DIR/*.err; do
     size=( $(stat --printf="%s" $err) )
-    if [[ $size == '0' ]]
+    if [[ $size == '0' ]] 
     then
-	# remove empty error files
 	rm $err
     else
-	# prints nodes that have errors to terminal
 	echo $err | cut -d'-' -f 2 | cut -d'/' -f 2
     fi
     
