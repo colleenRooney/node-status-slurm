c27197b8ed32006de07f9a54027d9e4627f45e1b
colleen-work <crooney@pdx.edu>
2018-11-28 15:51:37 -0800

    fixing comments

--- sinfo_parsing.py
+++ sinfo_parsing.py
@@ -6,31 +6,37 @@ import re
 def node_pretty(node_number, compute=True):
     """Given a string 'node_number' pads node number with 0s"""
     # this is because of how our nodes are named, need to change if nodes are
-    # named differently
+    # named differently compute nodes have 3 digits, phi and himem have 2
     length = 3 if compute else 2
     while len(node_number) < length:
         node_number = '0' + node_number
     return node_number
 
+
 def sep_nodes(node_type, partition, node_range, node_list=[]):
-    """Given a string 'node_type' (compute, phi, himem) and a string
-    'node_range' (001-031, 095) appends the name of all the nodes in the range,
-    or the single node in cases like 095 to a list (default empty list) and
-    returns the list"""
+    """Given a string 'node_type' (compute, phi, himem) a partition (medium,
+    long, phi etc.) and a string 'node_range' (001-031, 095) appends a
+    dictionary with the name stored under the key 'name' and the partition
+    stored under the key 'partition' of all the nodes in the range, to a list
+    (default empty list) and returns the list"""
     start_end = node_range.split('-')
+
     # janky fix... should really use regex incase name of default partition
     # changes
     if partition == "medium*": partition = "medium"
+
     if len(start_end) == 2:
         for node in range(int(start_end[0]), int(start_end[1]) + 1):
+            # specific to our cluster 
             compute = False if node_type != "compute" else True
             node_number = node_pretty(str(node), compute)
             node_list.append({'node':node_type + node_number,
-            'partition':partition})
+                                'partition':partition})
 
     elif len(start_end) == 1:
         node_number = node_pretty(str(start_end[0]))
         node_list.append(node_type + node_number)
+
 
 def get_idle_nodes(partition=''):
     """Returns the idle nodes using the SLURM sinfo command. If a partition
@@ -39,6 +45,8 @@ def get_idle_nodes(partition=''):
     grep_string = partition + '.*idle'
     sinfo_out = subprocess.Popen(['sinfo'], stdout=subprocess.PIPE)
 
+    # this could be fixed to work with other configurations, duplicates just
+    # need to be removed
     if partition != 'allcpu':
         no_allcpu = subprocess.Popen(['grep', '-v', 'allcpu'], stdin=sinfo_out.stdout,
         stdout=subprocess.PIPE)
@@ -56,10 +64,10 @@ def get_idle_nodes(partition=''):
             idle_lines.append({'nodes':partition_nodes, 'partition':partition})
 
     idle_nodes = []
-    for part in idle_lines:
-        # extract node type and numbers
-        match = re.match('((compute)|(himem)|(phi))\[(.*)\]', part['nodes'])
-        partition = part['partition']
+    for line in idle_lines:
+        # extract node type and numbers... specfic to our cluster
+        match = re.match('((compute)|(himem)|(phi))\[(.*)\]', line['nodes'])
+        partition = line['partition']
 
         node_list = match.group(5).split(',')
         node_type = match.group(1)
